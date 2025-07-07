225fe488af36fc18d31cf16d12c1c42065cc098f
Muthu2700405 <muthu270405@gmail.com>
2025-07-07 19:37:38 +0530

    items in cart is added

--- search.js
+++ search.js
@@ -5,6 +5,7 @@ console.log(prcont)
 search.addEventListener("keyup", function (event) {
     var enterval = event.target.value.toUpperCase();
     for (var i = 0; i < prcont.length; i++) {
+        event.preventDefault();
         var prname = prcont[i].querySelector('p').textContent.toUpperCase()
         if (prname.indexOf(enterval) < 0) {
             prcont[i].style.display = 'none';
@@ -12,5 +13,17 @@ search.addEventListener("keyup", function (event) {
             prcont[i].style.display = 'block';
         }
     }
-
+});
+search.addEventListener("keydown", function (event) {
+    if (event.key==='Enter'){
+    var enterval = event.target.value.toUpperCase();
+    for (var i = 0; i < prcont.length; i++) {
+        event.preventDefault();
+        var prname = prcont[i].querySelector('p').textContent.toUpperCase()
+        if (prname.indexOf(enterval) < 0) {
+            prcont[i].style.display = 'none';
+        } else {
+            prcont[i].style.display = 'block';
+        }
+    }}
 });
